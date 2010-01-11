3a12f2a29893408f32467bb133355991c3d4c379
keul <luca@keul.it>
2010-01-11 14:28:36 +0000

    Yet another fixes to handle the bach navigation and full page view

--- redturtle/alternative_orderui/browser/ordersupportui.js
+++ redturtle/alternative_orderui/browser/ordersupportui.js
@@ -4,6 +4,13 @@
 
 registerPloneFunction(function () {
 	var js_baseurl = jq("#js_baseurl").text();
+	var querystring = window.location.search;
+	var qspos = querystring.indexOf("pagenumber");
+	var qsn = '';
+	if (qspos>-1) {
+		// read the pagenumber argument
+		 qsn = "&pagenumber="+querystring.substr(qspos+11, qspos+12);
+	}
 	jq("#listing-table tr").find("td:first").each(function (i) {
 		jq(this).find("input:first").after('&nbsp;<span class="discreet order-index">'+(i+1)+'</span>');			
 	});
@@ -16,13 +23,17 @@ registerPloneFunction(function () {
 			          +'<img alt="" src="++resource++move_16x16.gif" />'
 					  +'</a>');
 			jq(".reorder-cmd", el).click(function(event) {
-				event.preventDefault();
-				var choosen = prompt("Inserisci la nuova posizione per l'elemento alla posizione "+i);
+				// event.preventDefault();
+				var choosen = prompt("Inserisci la nuova posizione per l'elemento alla posizione "+(i+1));
 				if (choosen != null) {
 					var v = parseInt(choosen)-1;
 					var upOrDown = (v<i?'up':'down');
 					var delta = (v<i?-(v-i):(v-i));
-					document.location.href = js_baseurl + "/folder_position?position="+upOrDown+"&amp;id="+el.attr("id").replace("cnt_","")+"&delta="+delta;
+					var show_all = "";
+					if (querystring.indexOf("show_all=true")>-1) {
+						show_all= "&show_all=true";
+					}
+					window.location.href = js_baseurl + "/folder_position?position="+upOrDown+"&amp;id="+el.attr("id").replace("cnt_","")+"&delta="+delta+show_all+qsn;
 				}
 			});
 		});
