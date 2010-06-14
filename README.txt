d5b8aceb4d54d7a290fba1d077dae1c3eaa79433
keul <luca@keul.it>
2010-06-14 14:15:52 +0000

    Egg clean and more infos

--- README.txt
+++ README.txt
@@ -4,3 +4,8 @@ Introduction
 An alternative *folder_contents* forms, that disable someway KSS ordering and show another fast way
 to reorder items (less cool, but this is working most of the time).
 
+Use this only when you (or your users) experience some problem with KSS ordering (not always this happens,
+but happens...).
+
+
+
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1.0'
+version = '0.1.1'
 
 setup(name='redturtle.alternative_orderui',
       version=version,
@@ -29,7 +29,8 @@ setup(name='redturtle.alternative_orderui',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
-      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       paster_plugins = ["ZopeSkel"],
       )
